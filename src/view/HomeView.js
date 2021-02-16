b3b2cca36a8707aebf4de23408e5380bdd82a3da
Mattis Erkensten <m.erkensten@icloud.com>
2021-02-16 14:48:48 +0100

    inlamning 3

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -18,7 +18,7 @@ export const HomeView = () => {
           <h2>name: {data.name}</h2>
           <h2>id: {data.id}</h2>
           <h2>weight: {data.weight}</h2>
-          <h2>height: {data.hegiht}</h2>
+          <h2>height: {data.height}</h2>
           <h2>type: {data.types[0].type.name}</h2>
         </div>
       );
